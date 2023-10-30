dfe61ba3c43e0f8cfb5314ed554b4e2a24193623
Ahn Jiwan <synoti21@naver.com>
2023-10-30 19:09:34 +0900

    :sparkles: feat: grafana & prometheus dashboard

---
+++ monitoring/prometheus/deploy.sh
@@ -0,0 +1,10 @@
+kubectl apply -f prometheus-cluster-role.yaml
+kubectl apply -f prometheus-config-map.yaml
+kubectl apply -f prometheus-deploy.yaml
+kubectl apply -f prometheus-node-exporter.yaml
+kubectl apply -f prometheus-svc.yaml
+
+kubectl apply -f kube-state-cluster-role.yaml
+kubectl apply -f kube-state-deploy.yaml
+kubectl apply -f kube-state-svcaccount.yaml
+kubectl apply -f kube-state-svc.yaml
