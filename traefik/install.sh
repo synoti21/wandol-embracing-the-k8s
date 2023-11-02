4a006cbe0b23750865804b92a2eb6fb9b92c29fb
Ahn Jiwan <synoti21@naver.com>
2023-11-03 04:31:00 +0900

    :sparkles: helm values.yaml for traefik

--- traefik/install.sh
+++ traefik/install.sh
@@ -1,7 +1,8 @@
 helm install traefik traefik/traefik \
   --namespace=traefik \
   --create-namespace \
-  --set="additionalArguments={--certificatesresolvers.${resolverName}.acme.tlschallenge,--certificatesresolvers.${resolverName}.acme.email={email},--certificatesresolvers.${resolverName}.acme.storage=/data/acme.json}" \
+  --set="additionalArguments={--certificatesresolvers.${resolverName}.acme.tlschallenge,--certificatesresolvers.${resolverName}.acme.email=${email},--certificatesresolvers.${resolverName}.acme.storage=/data/acme.json, --entrypoints.web.http.redirections.entryPoint.to=websecure, --entrypoints.web.http.redirections.entryPoint.scheme=https, --entrypoints.web.http.redirections.entrypoint.permanent=true, --api.dashboard=true}" \
   --set="service.type=NodePort" \
   --set="ports.websecure.tls.enabled=true"
+
 
