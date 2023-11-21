1356b73c88f50bb404bc9a14c67eea2256e53034
Ahn Jiwan <synoti21@naver.com>
2023-11-21 20:30:07 +0900

    :sparkles: feat: deploy sonarqube

--- argocd/install_argocd.sh
+++ argocd/install_argocd.sh
@@ -5,4 +5,4 @@ kubectl apply -f install.yaml
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
-kubectl patch svc argocd-server -n cicd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl patch svc argocd-server -n cicd -p '{"spec": {"type": "NodePort"}}'
